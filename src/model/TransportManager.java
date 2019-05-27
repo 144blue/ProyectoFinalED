845c595b6da4a7a3056af93c1ae14f21b3ffd472
144blue <aletru321@gmail.com>
2019-05-26 23:05:03 -0500

    update

--- src/model/TransportManager.java
+++ src/model/TransportManager.java
@@ -1,5 +1,31 @@
 package model;
 
+import graphV.GraphAlgorithms;
+import graphV.MatrixGraph;
+
 public class TransportManager {
 
+	private Station[] stations;
+	private GraphAlgorithms<Integer, Integer> algoritms; 
+	private MatrixGraph<Integer, Integer> graphNOCambas;
+	
+	public TransportManager() {
+		
+		algoritms =new GraphAlgorithms<>();
+		graphNOCambas= new MatrixGraph<>(true);
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	
+	}
+	
+	public void giveBestRoute() {
+		
+	}
+	
+	
+	
 }
