a157590b5d35142ef0cf54e0027995d4b281e2dc
vardo159 <braianelsociales@gmail.com>
2019-05-28 21:26:06 -0500

    update

--- structu/test/MatrixGraphTest.java
+++ structu/test/MatrixGraphTest.java
@@ -12,116 +12,116 @@ import interfaces.IGraph;
 
 class MatrixGraphTest {
 	
-IGraph<Integer,Integer> graphL;
+IGraph<Integer,Integer> graphM;
 	
 	public void scenario1() {
-		graphL=new MatrixGraph<>(true);
+		graphM=new MatrixGraph<>(true);
 	}
 	
 	public void scenario2() {
-		graphL=new MatrixGraph<>(true);
-		graphL.insertVertex(3);
-		graphL.insertVertex(4);
+		graphM=new MatrixGraph<>(true);
+		graphM.insertVertex(3);
+		graphM.insertVertex(4);
 	}
 	
 	public void scenario3() {
-		graphL=new ListGraph<Integer,Integer>(true);
-		graphL.insertEdge(4,3,4);
-		graphL.insertEdge(3,3,2);
-		graphL.insertEdge(2,3,5);
-		graphL.insertEdge(1,2,4);
-		graphL.insertEdge(3,4,7);
+		graphM=new ListGraph<Integer,Integer>(true);
+		graphM.insertEdge(4,3,4);
+		graphM.insertEdge(3,3,2);
+		graphM.insertEdge(2,3,5);
+		graphM.insertEdge(1,2,4);
+		graphM.insertEdge(3,4,7);
 	}
 	
 	public void scenario4() {
-		graphL=new ListGraph<Integer,Integer>(true);
-		graphL.insertVertex(3);
-		graphL.insertVertex(2);
-		graphL.insertVertex(1);
-		graphL.insertVertex(6);
-		graphL.insertVertex(9);
+		graphM=new ListGraph<Integer,Integer>(true);
+		graphM.insertVertex(3);
+		graphM.insertVertex(2);
+		graphM.insertVertex(1);
+		graphM.insertVertex(6);
+		graphM.insertVertex(9);
 	}
 
 
 	@Test
 	void test0() {
 		scenario1();
-		assertTrue(graphL.getAmountVertices()==0);
-		graphL.insertVertex(3);
-		graphL.insertVertex(1);
-		assertTrue(graphL.getAmountVertices()==2);
+		assertTrue(graphM.getAmountVertices()==0);
+		graphM.insertVertex(3);
+		graphM.insertVertex(1);
+		assertTrue(graphM.getAmountVertices()==2);
 	}
 	
 	@Test
 	void testIsGraphUndirected() {
 		scenario1();
-		assertTrue(graphL.isGraphUndirected());
+		assertTrue(graphM.isGraphUndirected());
 	}
 	
 
 	@Test
 	void testInserEdge() {
 		scenario2();
-		assertTrue(graphL.insertEdge(4,3,4));
-		assertTrue(graphL.insertEdge(3,3,2));
+		assertTrue(graphM.insertEdge(4,3,4));
+		assertTrue(graphM.insertEdge(3,3,2));
 	//	assertFalse(graphL.insertEdge(4,3,4));
-		assertTrue(graphL.insertEdge(2,3,7));
-		assertTrue(graphL.insertEdge(1,2,4));
-		assertTrue(graphL.insertEdge(5,2,7));
-		assertTrue(graphL.insertEdge(3,4,7));
+		assertTrue(graphM.insertEdge(2,3,7));
+		assertTrue(graphM.insertEdge(1,2,4));
+		assertTrue(graphM.insertEdge(5,2,7));
+		assertTrue(graphM.insertEdge(3,4,7));
 		//assertFalse(graphL.insertEdge(4,4,3));
-		assertTrue(graphL.insertEdge(7,5,7));
-		assertTrue(graphL.getEdges().size()==7);
+		assertTrue(graphM.insertEdge(7,5,7));
+		assertTrue(graphM.getEdges().size()==7);
 	}
 	
 	@Test
 	void testThereIsVertex() {
 		scenario3();
-		assertTrue(graphL.thereIsVertex(3));
-		assertTrue(graphL.thereIsVertex(4));
-		assertTrue(graphL.thereIsVertex(7));
-		assertTrue(graphL.thereIsVertex(5));
-		assertFalse(graphL.thereIsVertex(10));
-		assertFalse(graphL.thereIsVertex(1));
+		assertTrue(graphM.thereIsVertex(3));
+		assertTrue(graphM.thereIsVertex(4));
+		assertTrue(graphM.thereIsVertex(7));
+		assertTrue(graphM.thereIsVertex(5));
+		assertFalse(graphM.thereIsVertex(10));
+		assertFalse(graphM.thereIsVertex(1));
 		
 	}
 	
 	@Test
 	void testThereIsEdge() {
 		scenario3();
-		assertTrue(graphL.thereIsEdge(3,2));
-		assertTrue(graphL.thereIsEdge(2,4));
-		assertTrue(graphL.thereIsEdge(4,2));
-		assertTrue(graphL.thereIsEdge(4,7));
-		assertFalse(graphL.thereIsEdge(2,7));
-		assertFalse(graphL.thereIsEdge(5,7));
+		assertTrue(graphM.thereIsEdge(3,2));
+		assertTrue(graphM.thereIsEdge(2,4));
+		assertTrue(graphM.thereIsEdge(4,2));
+		assertTrue(graphM.thereIsEdge(4,7));
+		assertFalse(graphM.thereIsEdge(2,7));
+		assertFalse(graphM.thereIsEdge(5,7));
 		
 	}
 	
 	@Test
 	void testInsertVertex() {
 		scenario1();
-		graphL.insertVertex(3);
-		graphL.insertVertex(4);
-		graphL.insertVertex(7);
-		graphL.insertVertex(1);
-		graphL.insertVertex(9);
+		graphM.insertVertex(3);
+		graphM.insertVertex(4);
+		graphM.insertVertex(7);
+		graphM.insertVertex(1);
+		graphM.insertVertex(9);
 		
-		assertFalse(graphL.insertVertex(9));
-		assertTrue(graphL.insertVertex(10));
-		assertTrue(graphL.getAmountVertices()==6);
+		assertFalse(graphM.insertVertex(9));
+		assertTrue(graphM.insertVertex(10));
+		assertTrue(graphM.getAmountVertices()==6);
 	}
 	
 	@Test
 	void testGetAmountVertices() {
 		scenario4();
-		assertTrue(graphL.getAmountVertices()==5);
+		assertTrue(graphM.getAmountVertices()==5);
 	}
 	
 	@Test
 	void testgetValues() {
 		scenario4();
-		ArrayList<Integer> elems= graphL.getValues();
+		ArrayList<Integer> elems= graphM.getValues();
 		int[] expected= {1,2,3,6,9};
 		
 		for(int i=0;i<elems.size();i++)
