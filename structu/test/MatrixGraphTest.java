309713583c9f95d039b190d8223c42ea863f679c
vardo159 <braianelsociales@gmail.com>
2019-05-28 20:26:41 -0500

    update

--- structu/test/MatrixGraphTest.java
+++ structu/test/MatrixGraphTest.java
@@ -22,6 +22,14 @@ IGraph<Integer,Integer> graphL;
 		graphL.insertVertex(4);
 	}
 	
+	public void scenario3() {
+		graphL=new ListGraph<Integer,Integer>(true);
+		graphL.insertEdge(4,3,4);
+		graphL.insertEdge(3,3,2);
+		graphL.insertEdge(2,3,5);
+		graphL.insertEdge(1,2,4);
+		graphL.insertEdge(3,4,7);
+	}
 
 	@Test
 	void test0() {
@@ -46,6 +54,30 @@ IGraph<Integer,Integer> graphL;
 		//assertFalse(graphL.insertEdge(4,4,3));
 		assertTrue(graphL.insertEdge(7,5,7));
 		assertTrue(graphL.getEdges().size()==7);
+	}
+	
+	@Test
+	void testThereIsVertex() {
+		scenario3();
+		assertTrue(graphL.thereIsVertex(3));
+		assertTrue(graphL.thereIsVertex(4));
+		assertTrue(graphL.thereIsVertex(7));
+		assertTrue(graphL.thereIsVertex(5));
+		assertFalse(graphL.thereIsVertex(10));
+		assertFalse(graphL.thereIsVertex(1));
+		
+	}
+	
+	@Test
+	void testThereIsEdge() {
+		scenario3();
+		assertTrue(graphL.thereIsEdge(3,2));
+		assertTrue(graphL.thereIsEdge(2,4));
+		assertTrue(graphL.thereIsEdge(4,2));
+		assertTrue(graphL.thereIsEdge(4,7));
+		assertFalse(graphL.thereIsEdge(2,7));
+		assertFalse(graphL.thereIsEdge(5,7));
+		
 	}
 	
 	@Test
